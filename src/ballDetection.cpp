53cb537f59c7b090983869c96dfa9e8431a8e6ea
matteodalnevo <matteodn59@gmail.com>
2024-06-05 11:47:23 +0200

    ROI

--- src/ballDetection.cpp
+++ src/ballDetection.cpp
@@ -5,13 +5,13 @@
 cv::Mat ballDetection(const cv::Mat& image) {
 
     // Convert image to grayscale
-    cv::Mat gray(image.rows, image.cols, CV_8UC1);  // Create a grayscale image matrix
+    cv::Mat gray;  // Create a grayscale image matrix
     cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);  // Convert the input image to grayscale
-    cv::GaussianBlur(gray, gray, cv::Size(3, 3), 2, 2);  // Apply Gaussian blur to reduce noise
+    cv::GaussianBlur(gray, gray, cv::Size(5, 5), 2, 2);  // Apply Gaussian blur to reduce noise
 
     // detecting circles via Hough transform
     std::vector<cv::Vec3f> circles;  // Vector to store detected circles
-    cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1, gray.rows/32, 50, 13, 6, 10);  // Detect circles using Hough Transform
+    cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1, gray.rows/32, 50, 13, 6, 14);  // Detect circles using Hough Transform
 
     // Create an overlay image
     cv::Mat overlay;
@@ -25,6 +25,10 @@ cv::Mat ballDetection(const cv::Mat& image) {
         // Define the bounding box
         cv::Rect boundingBox(center.x - radius, center.y - radius, radius * 2, radius * 2);
 
+        //cv::Mat dst = image(boundingBox);
+        //cv::imshow("roi", dst);
+        //cv::waitKey(0);
+
         // Draw the bounding box
         cv::rectangle(image, boundingBox, cv::Scalar(0, 0, 255), 1);
         // Fill it
@@ -32,7 +36,7 @@ cv::Mat ballDetection(const cv::Mat& image) {
 
         // std::cout << radius << std::endl;
     }
-    
+
     // Blend the overlay with the original image
     double alpha = 0.5; // Transparency factor
     cv::addWeighted(overlay, alpha, image, 1 - alpha, 0, image);
--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@
 int main() {
 
     // Load the dataset game 1
-    cv::Mat image_frame_first = cv::imread("../data/game1_clip1/frames/frame_first.png");
-    cv::Mat image_frame_last = cv::imread("../data/game1_clip1/frames/frame_last.png");
+    cv::Mat image_frame_first = cv::imread("../data/game2_clip1/frames/frame_first.png");
+    cv::Mat image_frame_last = cv::imread("../data/game2_clip1/frames/frame_last.png");
 
-    cv::Mat mask_frame_first = cv::imread("../data/game1_clip1/masks/frame_first.png", cv::IMREAD_ANYDEPTH);
-    cv::Mat mask_frame_last = cv::imread("../data/game1_clip1/masks/frame_last.png", cv::IMREAD_ANYDEPTH);
+    cv::Mat mask_frame_first = cv::imread("../data/game3_clip2/masks/frame_first.png", cv::IMREAD_ANYDEPTH);
+    cv::Mat mask_frame_last = cv::imread("../data/game3_clip2/masks/frame_last.png", cv::IMREAD_ANYDEPTH);
 
     /*
     std::vector<BoundingBox> bbox_frame_first;
@@ -26,12 +26,15 @@ int main() {
             std::cout << bbox.x << " " << bbox.y << " " << bbox.width << " " << bbox.height << " " << bbox.ID << std::endl;
         }
     }
+    */
 
     // Create color image
     cv::Mat colorImage;
     // Call the function to segment the mask
     mapGrayscaleMaskToColorImage(mask_frame_first, colorImage);
-    */
+    
+    // Call the function to show an image
+    showImage(colorImage, "image");
 
     // Test the functions
     cv::Mat result = ballDetection(image_frame_first);
