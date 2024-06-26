44b19e1f902f565fead893bf65ed85dbb5783c7d
matteodalnevo <matteodn59@gmail.com>
2024-06-26 22:00:25 +0200

    ROI of table for ball det

--- src/ballDetection.cpp
+++ src/ballDetection.cpp
@@ -1,7 +1,7 @@
 #include "ballDetection.h"
 #include <iostream>
 
-// Usse the corners
+// Use the corners
 cv::Mat ballDetection(const cv::Mat& image, std::vector<cv::Point> vertices) {
     
     // Make a copy of the input image to draw the rectangle on
@@ -10,7 +10,17 @@ cv::Mat ballDetection(const cv::Mat& image, std::vector<cv::Point> vertices) {
     // Draw the quadrilateral by connecting the vertices
     std::vector<std::vector<cv::Point>> contours;
     contours.push_back(vertices);
-    cv::polylines(result, contours, true, cv::Scalar(0, 255, 255), 3); // Green color with thickness 2
+    cv::polylines(result, contours, true, cv::Scalar(0, 255, 255), 2); // Green color with thickness 2
+    
+    // Create a mask for the ROI
+    cv::Mat mask = cv::Mat::zeros(image.size(), CV_8UC1); // Initialize mask with zeros (black)
 
-    return result;
+    // Fill the ROI (region of interest) defined by the vertices with white color (255)
+    cv::fillPoly(mask, contours, cv::Scalar(255));
+
+    // Create a masked image using the original image and the mask
+    cv::Mat maskedImage;
+    image.copyTo(maskedImage, mask);
+
+    return maskedImage;
 }
